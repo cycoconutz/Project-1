e24227c056c8c29eb27bac7cae8cf13589c3c80f
Stephen Kelly <sbk13@my.fsu.edu>
2022-04-14 10:04:14 -0500

    added some changes to the JS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,8 @@
-var sportsButtonsEl = document.querySelector('#sports-buttons');
-var teamsEl = document.querySelector('#teams');
-var dateEl = document.querySelector('#date');
-var timeEl = document.querySelector('#time');
-
+var sportsButtonsEl = document.querySelector("#sports-buttons");
+var teamsEl = document.querySelector("#teams");
+var dateEl = document.querySelector("#date");
+var timeEl = document.querySelector("#time");
+var recipes = document.getElementsByClassName("carousel-item");
 
 //Button listeners for sports API call
 var sportsFormSubmitHandler = function (event) {
@@ -12,54 +12,63 @@ var sportsFormSubmitHandler = function (event) {
   sports(drink);
 };
 
-var displaycocktails = function (name, type)
-{
-  if (name==="") { var cocktailApi = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + type;
-  console.log(cocktailApi)
+var getcocktails = function (name, type) {
+  if (name === "") {
+    var cocktailApi =
+      "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + type;
+    console.log(cocktailApi);
   }
 
-  if (type==="Search By Liqour") {var cocktailApi = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?s=' + name;
-  console.log(cocktailApi);
+  if (type === "Select a Liquor") {
+    var cocktailApi =
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + name;
+    console.log(cocktailApi);
   }
+
   fetch(cocktailApi).then(function (response) {
-    if (response.ok) {
-      console.log(response);
-      response.json().then(function (data) {
-        console.log(data);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  })
-  
+    response.json().then(function (data) {
+      populatecocktails(data);
+    });
+  });
 };
 
-$('#submit-form').on('click', function(event) {
+var populatecocktails = function (data1) {
+  var drinksArray = data1.drinks;
+  var randomrecipe = Math.floor(Math.random() * drinksArray.length);
+  // for (var i = 0; i > recipes.length; i++) {
+    
+    recipes[0].src = drinksArray[0].strDrinkThumb;
+
+  //  recipes[i].src= drinksArray[randomrecipe].strDrinkThumb;
+
+  //   console.log(randomrecipe);
+  // }
+};
+
+$("#submit-form").on("click", function (event) {
   event.preventDefault();
-  var text =$('#username').val().trim();
-  var liqour =$('#typeliq option:selected').text()
-displaycocktails(text, liqour);
-})
-
-
-
-
-
-
+  var text = $("#username").val().trim();
+  var liqour = $("#typeliq option:selected").text();
+  getcocktails(text, liqour);
+});
 
 //Sports API call
 var sports = function (selectedSport) {
-  if (selectedSport === 'baseball-btn') {
-    var sportsApiUrl = 'https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4424&s=2022'
+  if (selectedSport === "baseball-btn") {
+    var sportsApiUrl =
+      "https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4424&s=2022";
   }
-  if (selectedSport === 'football-btn') {
-    var sportsApiUrl = 'https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4391&s=2021'
+  if (selectedSport === "football-btn") {
+    var sportsApiUrl =
+      "https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4391&s=2021";
   }
-  if (selectedSport === 'soccer-btn') {
-    var sportsApiUrl = 'https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4328&s=2021-2022'
+  if (selectedSport === "soccer-btn") {
+    var sportsApiUrl =
+      "https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4328&s=2021-2022";
   }
-  if (selectedSport === 'basketball-btn') {
-    var sportsApiUrl = 'https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4387&s=2021-2022'
+  if (selectedSport === "basketball-btn") {
+    var sportsApiUrl =
+      "https://www.thesportsdb.com/api/v1/json/2/eventsseason.php?id=4387&s=2021-2022";
   }
 
   fetch(sportsApiUrl).then(function (response) {
@@ -67,8 +76,7 @@ var sports = function (selectedSport) {
       console.log(data);
       populateSchedule(data);
     });
-  })
-
+  });
 };
 
 function populateSchedule(data) {
@@ -77,38 +85,35 @@ function populateSchedule(data) {
   dateEl.innerHTML = "";
 
   for (i = 99; i >= 90; i--) {
-    var teams = data.events[i].strEventAlternate
-    var date = data.events[i].dateEvent
-    var time = data.events[i].strTime
-    console.log(teams)
-    console.log(date)
-    console.log(time)
-    var teamsChild = document.createElement('h5')
-    var timeChild = document.createElement('h5')
-    var dateChild = document.createElement('h5')
-    teamsChild.textContent = teams
+    var teams = data.events[i].strEventAlternate;
+    var date = data.events[i].dateEvent;
+    var time = data.events[i].strTime;
+    console.log(teams);
+    console.log(date);
+    console.log(time);
+    var teamsChild = document.createElement("h5");
+    var timeChild = document.createElement("h5");
+    var dateChild = document.createElement("h5");
+    teamsChild.textContent = teams;
     teamsEl.appendChild(teamsChild);
-    timeChild.textContent = time
+    timeChild.textContent = time;
     timeEl.appendChild(timeChild);
-    dateChild.textContent = date
+    dateChild.textContent = date;
     dateEl.appendChild(dateChild);
-  };
-
+  }
 }
 
-
-sportsButtonsEl.addEventListener('click', function (event) {
-  var selectedSport = event.target.id
+sportsButtonsEl.addEventListener("click", function (event) {
+  var selectedSport = event.target.id;
   sports(selectedSport);
 });
-
 
 //Initial functions on page load
 function init() {
   $(document).ready(function () {
-    $('select').formSelect();
-    $('.parallax').parallax();
-    $('.carousel').carousel();
+    $("select").formSelect();
+    $(".parallax").parallax();
+    $(".carousel").carousel();
   });
 }
 init();
